2a00fb3362b223c3003650954ce5ee6410617feb
gmalho <gaurav.malhotra@nike.com>
2019-01-04 15:58:54 +0100

    refactoring

--- src/main/java/com/gonnect/spark/drools/worker/SparkWorker.java
+++ src/main/java/com/gonnect/spark/drools/worker/SparkWorker.java
@@ -19,23 +19,7 @@ public class SparkWorker {
     public static void doExecuteSparkWithDrools() {
 
         //Setup data
-        List<Person> anInputData = asList(
-                new Person(1, "firstName1", "lastName1", 10000, 568),
-                new Person(2, "firstName2", "lastName2", 12000, 654),
-                new Person(3, "firstName3", "lastName3", 100, 568),
-                new Person(4, "firstName4", "lastName4", 1000000, 788),
-                new Person(5, "firstName5", "lastName5", 10, 788),
-                new Person(6, "firstName6", "lastName6", 34000, 900),
-                new Person(7, "firstName7", "lastName7", 12000, 457),
-                new Person(8, "firstName8", "lastName8", 10000, 300),
-                new Person(9, "firstName9", "lastName9", 20000, 721),
-                new Person(10, "firstName10", "lastName10", 30000, 890),
-                new Person(11, "firstName11", "lastName11", 50000, 490),
-                new Person(12, "firstName12", "lastName12", 60000, 690),
-                new Person(13, "firstName13", "lastName13", 25000, 390),
-                new Person(14, "firstName14", "lastName14", 80000, 290)
-
-        );
+        List<Person> anInputData = getInputData();
         //Use all cores
         SparkConf sparkConf = new SparkConf()
                                 .setAppName("Spring Boot + Spark Job + Drools Application")
@@ -57,10 +41,12 @@ public class SparkWorker {
         sparkContext.close();
     }
 
+    //~~ Helper functions
+
     /**
      * Load the Rules (PersonApprovalRule.drl) in the container
      */
-    public static KieBase loadRules() {
+    private  KieBase loadRules() {
         KieServices kieServices = KieServices.Factory.get();
         KieContainer kieContainer = kieServices.getKieClasspathContainer();
         return kieContainer.getKieBase();
@@ -69,9 +55,29 @@ public class SparkWorker {
     /**
      * Fire/apply the rules if the credit score > 600
      */
-    public static Person doExecuteRules(KieBase base, Person a) {
+    private static Person doExecuteRules(KieBase base, Person a) {
         StatelessKieSession session = base.newStatelessKieSession();
         session.execute(a);
         return a;
+    }
+
+    private static List<Person> getInputData() {
+        return asList(
+                new Person(1, "firstName1", "lastName1", 10000, 568),
+                new Person(2, "firstName2", "lastName2", 12000, 654),
+                new Person(3, "firstName3", "lastName3", 100, 568),
+                new Person(4, "firstName4", "lastName4", 1000000, 788),
+                new Person(5, "firstName5", "lastName5", 10, 788),
+                new Person(6, "firstName6", "lastName6", 34000, 900),
+                new Person(7, "firstName7", "lastName7", 12000, 457),
+                new Person(8, "firstName8", "lastName8", 10000, 300),
+                new Person(9, "firstName9", "lastName9", 20000, 721),
+                new Person(10, "firstName10", "lastName10", 30000, 890),
+                new Person(11, "firstName11", "lastName11", 50000, 490),
+                new Person(12, "firstName12", "lastName12", 60000, 690),
+                new Person(13, "firstName13", "lastName13", 25000, 390),
+                new Person(14, "firstName14", "lastName14", 80000, 290)
+
+        );
     }
 }
